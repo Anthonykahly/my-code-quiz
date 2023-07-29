e6eff8dd4d3ef1179318a0e1c5d65ec964df202c
A K <akahly@gmail.com>
2023-07-29 14:02:13 -0400

    adding console logs

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,8 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_actual = document.querySelector(".quiz_actual");
 const options_li = document.querySelector(".options_li");
-const timerCount = quiz_actual.querySelector(".timer .timer_sec")
-
+const timerCount = quiz_actual.querySelector(".timer .timer_sec");
+ 
 
 // setting terms for start button being clicked
 start_btn.onclick = () => {
@@ -30,9 +30,13 @@ let q_count = 0 //Question count
 let q_numb = 1 //Question number (1-5)
 let counter;
 let timeV = 10; //Setting time value
+let useScore = 0;
 
-//Calling out the next button
+//Next button call
 const next_btn = quiz_actual.querySelector("footer .next_btn");
+const results = document.querySelector(".results"); //Results
+const restart_quiz = results.querySelector(".buttons .restart"); //Quiz restart function
+const quit_quiz = results.querySelector(".buttons .quit"); //Quiz quit functioning
 
 //Setting up the functioning on click settings for the next button
 next_btn.onclick = () => {
@@ -44,7 +48,8 @@ next_btn.onclick = () => {
         clearInterval(counter); //On click of nextbutton resets timer
         startTimer(timeV); //Then starts a new timer for 10 seconds
     } else {
-        console.log("Quiz Completed"); //Logs when the entire array of questions has been completed
+        console.log("All questions completed"); //Logs when the entire array of questions has been completed
+        showResultBox();
     }
 }
 
@@ -73,11 +78,14 @@ let checkIconTag = '<div class="icon tick"><span>&#10003;</span></i></div>';
 let xIconTag = '<div class="icon cross"><span>&#x2717;</span></i></div>';
 
 function optionSelected(answer) { //Setting up the function for user selecting quiz answers
+    clearInterval(counter); //Stops the timer when an answer is selected
     let userAnswer = answer.textContent;
     let correctAnswer = questions[q_count].answer;
     let allOptions = options_li.children.length;
     console.log(userAnswer) //Logs the users input for answer
     if (userAnswer == correctAnswer) {
+        useScore += 1;
+        console.log(useScore); //logs users score for correct answers
         answer.classList.add("correct"); //Set the option to change to the ".options.correct" option defined in CSS
         console.log("Answer Correct"); //Simple console log of correct answer for tracking functionality might remove later
         answer.insertAdjacentHTML("beforeend", checkIconTag) //Inserts a green checkmark when user selection is correct answer
@@ -100,12 +108,23 @@ function optionSelected(answer) { //Setting up the function for user selecting q
     }
 }
 
+function showResultBox() {
+    info_box.classList.remove("activeInfo"); //hides info box
+    quiz_actual.classList.remove("activeQuiz"); //hides quiz box
+    results.classList.add("activeResults"); //shows results box
+}
+
+
 //starting timer function 10sec per question
 function startTimer(time) {
     counter = setInterval(timer, 1000);
     function timer(){
         timerCount.textContent = time;
         time--;
+        if(time < 0) {
+            clearInterval(counter);
+            timerCount.textContent = "0";
+        }
     }
 }
 
@@ -114,7 +133,7 @@ function startTimer(time) {
 
 
 function qCounter(index) {
-    const bottom_q_counter = quiz_actual.querySelector(".total_q");
+    const bottom_q_counter = quiz_actual.querySelector(".total_q"); //Creating a span tag to counter question number and total questions
     let totalQCountTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>';
     bottom_q_counter.innerHTML = totalQCountTag;
 }
